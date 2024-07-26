6bdccc39007db94d073ed04c6b97d886e83583e2
Kamal <kamal0000027@gmail.com>
2024-07-26 21:04:19 +0530

    "recreate my folders"

---
+++ BasicsOfJs/1_LetVarConst.js
@@ -0,0 +1,12 @@
+const CollegeRollNumber = 22827
+let MyName = "Kamal"
+var CollegeName = "PIET"
+// if we create hai variable and can not declare a value then javascript give the output it is undefined
+// CourseYear = "3rd Year" this is also correct because javascript allow to declare your variable without use a keyword but it is not good practice
+
+// CollegeName = "KUK" This is Right because let have re declare and it have hai block scope
+// CollegeRollNumber = 22828 it can not re declare because it is a const variable it is a global scope
+
+
+
+console.table([CollegeName , MyName , CollegeRollNumber , CourseYear])
---
+++ BasicsOfJs/2_DataTypesAndECMAStandard.js
@@ -0,0 +1,15 @@
+"use strict" ;  // it is used to give the instruction to javascript engine because we use new javascript 
+
+// Data Types in JS
+
+// number
+// null
+// string
+// symbol
+// undefined
+// bigint
+// Boolean
+
+console.log(typeof "kamal" , typeof (2) , typeof null , typeof undefined , typeof true )
+// type of null is Object Some people says it is a mistake and some says it is right
+// type of undefined is undefined
