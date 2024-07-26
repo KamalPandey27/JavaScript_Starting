c5d62e35f4b862c6d671a606b388ba907cbdda1f
Kamal <kamal0000027@gmail.com>
2024-07-26 22:42:08 +0530

    "basic js"

--- BasicsOfJs/2_DataTypesAndECMAStandard.js
+++ BasicsOfJs/2_DataTypesAndECMAStandard.js
@@ -13,3 +13,35 @@
 console.log(typeof "kamal" , typeof (2) , typeof null , typeof undefined , typeof true )
 // type of null is Object Some people says it is a mistake and some says it is right
 // type of undefined is undefined
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
---
+++ BasicsOfJs/3_DataTypeConversion.js
@@ -0,0 +1,76 @@
+// let num = "33"
+// let convertnum = Number(num)
+// console.log(convertnum)
+// console.log(typeof convertnum)
+// it will give a number 
+
+
+// num = "kamal27"
+// let convertnum = Number(num)
+// console.log(convertnum)
+// console.log(typeof convertnum)
+// this type is number but it will give a output is NaN because it is not a number 
+
+
+// let str = 27
+// let strToString = String(str)
+// console.log(strToString);
+// console.log(typeof strToString);
+// it convert this number into string and give the output in numbers but its typeof is string
+
+
+
+// let boolean = 1
+// let booleanValue = Boolean(boolean)
+// console.log(booleanValue);
+// console.log(typeof booleanValue);
+// it will give a typeof is boolean because it convert the value in boolean and its output is true
+
+// let boolean = "1"
+// let booleanValue = Boolean(boolean)
+// console.log(booleanValue);
+// console.log(typeof booleanValue)
+// it also give the sameoutput
+// but when we convert the other value in boolean except 0  then it will show always true
+
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
