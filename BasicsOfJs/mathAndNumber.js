c9402f88dac0c9f963f0c08419345e193653bb1d
Kamal <kamal0000027@gmail.com>
2024-07-27 20:29:50 +0530

    add file topic math and number

---
+++ BasicsOfJs/mathAndNumber.js
@@ -0,0 +1,42 @@
+let num = 900
+// console.log(num)
+
+// if i want only value in number
+
+let num2 = new Number(22827)
+// console.log(num2)
+
+
+// console.log(num2.toString().length);
+// console.log(num2.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+
+// console.log(Math)
+// console.log((Math.random()*10) + 1);
+// console.log(Math.round(Math.random()*10) +1) // Math.round() it is used to get of round of number 
+// console.log(Math.floor(Math.random()*10)+1) // Math.floor() it is used to get of lowest round  of number 
+// console.log(Math.ceil(4.2)) // it will give the maximum roundof value 
+
+let arr = [1,2,3,4,5,6]
+let min = Math.min(...arr)
+// console.log(min)
+
+
+let ar2 = [1,2,3,4,5,6]
+let max = Math.max(...ar2)
+// console.log(max)
+
+
+
