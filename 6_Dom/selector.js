4f5974c7ea51c2cec7f5bdaf00286ed672e60a28
Kamal <kamal0000027@gmail.com>
2024-07-30 00:53:42 +0530

    Document object model

---
+++ 6_Dom/selector.js
@@ -0,0 +1,14 @@
+// innertext give the only text of a Element which is available
+// innercontext it will give the hidden text also 
+//  innerhtml it will give the whole innerhtml of a element also a tag 
+
+// document.querySelectorAll it will give the node list which  little nature match  to the array 
+// but not all method of array is impliment on this node list if we use all method of array on node list then 
+// you will convet this to array by use of Array.from()
+
+// document.getelementbyclass() it will give the html collection and we can not use any array method to html collection
+// so we always convert this to array
+
+
+
+
