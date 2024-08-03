2a76ed60b0b0e8bdbb6391d446cf9ef02ac45653
Kamal <kamal0000027@gmail.com>
2024-08-03 21:47:26 +0530

    fetch and promises

---
+++ 8_ApiTalksAndPromises/Promises.js
@@ -0,0 +1,214 @@
+let promiseOne = new Promise((resolve,reject)=>{
+    let error = true
+    if(!error){
+        resolve("Your Request is resolve")
+    }
+    else{
+        reject("something went wrong")
+    }
+})
+
+promiseOne.then((value)=>{
+    console.log(value)
+}).catch((error)=>{
+    console.log(error)
+})
+.finally(() => console.log("The promise is either resolved or rejected"))
+
+// Finaly is always executed
+
+
+let fetchapi = fetch('https://jsonplaceholder.typicode.com/users')
+//  fetch always executed first because it is a special  queue or priority queue
+// fetch response a promise
+//  to see all working of fetch read documentation
+fetchapi.then((value) => {
+    return value.json()
+})
+.then((data) => {
+    console.log(data)
+    // console.log(data[0].name)
+})
+.catch((error) => {
+        console.log(error)
+    })
+
+
+const promiseFive = new Promise(function(resolve, reject){
+        setTimeout(function(){
+            let error = true
+            if (!error) {
+                resolve({username: "javascript", password: "123"})
+            } else {
+                reject('ERROR: JS went wrong')
+            }
+        }, 1000)
+    });
+    
+    async function consumePromiseFive(){
+        try {
+            const response = await promiseFive
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+    consumePromiseFive()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // const promiseOne = new Promise(function(resolve, reject){
+        //Do an async task
+        // DB calls, cryptography, network
+    //     setTimeout(function(){
+    //         console.log('Async task is compelete');
+    //         resolve()
+    //     }, 1000)
+    // })
+    
+    // promiseOne.then(function(){
+    //     console.log("Promise consumed");
+    // })
+    
+    // new Promise(function(resolve, reject){
+    //     setTimeout(function(){
+    //         console.log("Async task 2");
+    //         resolve()
+    //     }, 1000)
+    
+    // }).then(function(){
+    //     console.log("Async 2 resolved");
+    // })
+    
+    // const promiseThree = new Promise(function(resolve, reject){
+    //     setTimeout(function(){
+    //         resolve({username: "Chai", email: "chai@example.com"})
+    //     }, 1000)
+    // })
+    
+    // promiseThree.then(function(user){
+    //     console.log(user);
+    // })
+    
+    // const promiseFour = new Promise(function(resolve, reject){
+    //     setTimeout(function(){
+    //         let error = true
+    //         if (!error) {
+    //             resolve({username: "hitesh", password: "123"})
+    //         } else {
+    //             reject('ERROR: Something went wrong')
+    //         }
+    //     }, 1000)
+    // })
+    
+    //  promiseFour
+    //  .then((user) => {
+    //     console.log(user);
+    //     return user.username
+    // }).then((username) => {
+    //     console.log(username);
+    // }).catch(function(error){
+    //     console.log(error);
+    // }).finally(() => console.log("The promise is either resolved or rejected"))
+    
+    
+    
+    // const promiseFive = new Promise(function(resolve, reject){
+    //     setTimeout(function(){
+    //         let error = true
+    //         if (!error) {
+    //             resolve({username: "javascript", password: "123"})
+    //         } else {
+    //             reject('ERROR: JS went wrong')
+    //         }
+    //     }, 1000)
+    // });
+    
+    // async function consumePromiseFive(){
+    //     try {
+    //         const response = await promiseFive
+    //         console.log(response);
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+    
+    // consumePromiseFive()
+    
+    // async function getAllUsers(){
+    //     try {
+    //         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    
+    //         const data = await response.json()
+    //         console.log(data);
+    //     } catch (error) {
+    //         console.log("E: ", error);
+    //     }
+    // }
+    
+    //getAllUsers()
+    
+    // fetch('https://api.github.com/users/hiteshchoudhary')
+    // .then((response) => {
+    //     return response.json()
+    // })
+    // .then((data) => {
+    //     console.log(data);
+    // })
+    // .catch((error) => console.log(error))
+    
+    // promise.all
+    // yes this is also available, kuch reading aap b kro.
