8b79081c8b26287806dabb9e7ba76f2fb495c808
kamalpandey27 <kamal0000027@gmail.com>
2025-03-06 13:34:35 +0530

    c

--- 3_BasicOfJs/1_Function.js
+++ 3_BasicOfJs/1_Function.js
@@ -1,61 +1,55 @@
-
 function addnumber(Number1, Number2) {
-    if (!isNaN(Number1) && !isNaN(Number2)) {
-        return Number1 + Number2
-    }
-    else {
-        return "you Enter a Wrong number"
-    }
+  if (!isNaN(Number1) && !isNaN(Number2)) {
+    return Number1 + Number2;
+  } else {
+    return "you Enter a Wrong number";
+  }
 }
 
-let result = addnumber(2, 4)
+let result = addnumber(2, 4);
 // console.log(result)
-
+// After return the function will ignore the code which is return after the return statement
 
 function islogged(username = "Unknown User") {
-    if (username === undefined) {
-        return console.log("Enter a username")
-    }
-    else {
-        return `${username} Logged In`
-    }
+  if (username === undefined) {
+    return console.log("Enter a username");
+  } else {
+    return `${username} Logged In`;
+  }
 }
 
-let result1 = islogged()
+let result1 = islogged();
 // console.log(result1)
-
 
 // When we want to pass multiple value in function example a cart in shopping web app
 
-function cart(...price){
-    return price
+function cart(...price) {
+  return price;
 }
-
+//It can give a array and this operator is a rest operator but when we use this operator in obj and array then its name is spread operator
 // console.log( cart(300 , 2313 , 3211, 24532))
 
-// pass an object in function
+// *Pass an object in function
 
 const user = {
-    name: "kamal",
-    price : 321
+  name: "kamal",
+  price: 321,
+};
+
+function userdetails(anyobject) {
+  return `${anyobject.name} ${anyobject.price}`;
 }
 
-function userdetails(anyobject){
-
-return `${anyobject.name} ${anyobject.price}`
-}
-
-let result2 = userdetails(user)
-console.log(result2)
-
+let result2 = userdetails(user);
+console.log(result2);
 
 // pass a array in function
 
-let arr = [200,2341,6534,87651,23]
+let arr = [200, 2341, 6534, 87651, 23];
 
-function getarray(array){
-    console.log(array)
-    console.log(array[4])
+function getarray(array) {
+  console.log(array);
+  console.log(array[4]);
 }
 
-getarray(arr)
+getarray(arr);
--- 3_BasicOfJs/2_Scopes.js
+++ 3_BasicOfJs/2_Scopes.js
@@ -1,68 +1,66 @@
 // value declare outside the scope {} then it is a global scope variable
 
-
-let a = 42
-const b = 4342
+let a = 42;
+const b = 4342;
 // var c = "keyboard" // var is not used because its nature is global even if it is in scope or not
 
 if (true) {
-    //  this is a block scope
-    let a = "Kamal"
-    const b = "anmol"
-    console.log(a)
-    console.log(b)
+  //  this is a block scope
+  let a = "Kamal";
+  const b = "anmol";
+  console.log(a);
+  console.log(b);
 }
-console.log(a)
-console.log(b)
+// console.log(a)
+// console.log(b)
 // console.log(c)
-
 
 // deep dive in scope
 
+function one() {
+  const username = "hitesh";
 
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  //   console.log(website);
 
-function one(){
-    const username = "hitesh"
-
-    function two(){
-        const website = "youtube"
-        console.log(username);
-    }
-    // console.log(website);
-
-     two()
-
+  two();
 }
 
-// one()
+one();
 
 if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
-        const website = " youtube"
-        // console.log(username + website);
-    }
-    // console.log(website);
+  const username = "hitesh";
+  if (username === "hitesh") {
+    const website = " youtube";
+    // console.log(username + website);
+  }
+  // console.log(website);
 }
 
 // console.log(username);
 
-
 // ++++++++++++++++++ interesting ++++++++++++++++++
 
+console.log(addone(5));
 
-console.log(addone(5))
-
-function addone(num){
-    return num + 1
+function addone(num) {
+  return num + 1;
 }
 
-
-
-addTwo(5) 
+addTwo(5);
 // this will give  a error because we initialize a function in variable and a variable can not access before initialization
-const addTwo = function(num){
-    return num + 2
-}
+const addTwo = function (num) {
+  return num + 2;
+};
 
+// let ab = "Kamal";
 
+// if (ab) {
+//   ab += "Pandey";
+//   console.log(ab);
+// }
+
+// console.log(ab)
