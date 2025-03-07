8035e501e3446d68055f1ab6e3c0a3c4cba7c9ef
kamalpandey27 <kamal0000027@gmail.com>
2025-03-07 12:28:11 +0530

    Dom Completed

--- 6_Dom/1_selector.js
+++ 6_Dom/1_selector.js
@@ -1,14 +1,18 @@
 // innertext give the only text of a Element which is available
-// innercontext it will give the hidden text also 
-//  innerhtml it will give the whole innerhtml of a element also a tag 
+// innercontext it will give the hidden text also
+//  innerhtml it will give the whole innerhtml of a element also a tag
 
-// document.querySelectorAll it will give the node list which  little nature match  to the array 
-// but not all method of array is impliment on this node list if we use all method of array on node list then 
-// you will convet this to array by use of Array.from()
+// document.querySelectorAll it will give the node list which  little nature match  to the array
+// but not all method of array is impliment on this node list if we use all method of array on node list then
+// you will convert this to array by use of Array.from()
 
 // document.getelementbyclass() it will give the html collection and we can not use any array method to html collection
 // so we always convert this to array
 
+// window is object , it is also known as as global object
+// =>DOM
+// In simple words, the DOM (Document Object Model) is a way for web browsers to understand and manage the content of a web page.
 
+// When a web page loads, the browser turns the HTML code into a tree-like structure of objects, where each part of the page (like paragraphs, images, buttons, and links) becomes a node in this tree. JavaScript can then interact with this tree to change the content, style, or structure of the page without needing to reload it.
 
-
+// So, the DOM is like a bridge between the HTML you write and what you see and interact with on the screen. 😊
