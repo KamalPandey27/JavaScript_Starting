9feab1614601383f4f10a12b725cee40fead1ef9
kamalpandey27 <kamal0000027@gmail.com>
2025-03-11 23:10:03 +0530

    object oriented programming

--- 9_ObjectOrientedprogramming/opps.js
+++ 9_ObjectOrientedprogramming/opps.js
@@ -1,37 +1,42 @@
 const user = {
-    username: "hitesh",
-    loginCount: 8,
-    signedIn: true,
+  username: "Kamal",
+  loginCount: 8,
+  signedIn: true,
 
-    getUserDetails: function(){
-        //console.log("Got user details from database");
-        // console.log(`Username: ${this.username}`);
-        console.log(this);
-    }
-
-}
-
-
+  getUserDetails: function () {
+    //console.log("Got user details from database");
+    // console.log(`Username: ${this.username}`);
+    console.log(this);
+  },
+};
 
 //console.log(user.username)
 // console.log(user.getUserDetails());
 // console.log(this);
 
+function User(username, loginCount, isLoggedIn) {
+  this.username = username;
+  this.loginCount = loginCount;
+  this.isLoggedIn = isLoggedIn;
 
-function User(username, loginCount, isLoggedIn){
-    this.username = username;
-    this.loginCount = loginCount;
-    this.isLoggedIn = isLoggedIn
+  this.greeting = function () {
+    console.log(`Welcome ${this.username}`);
+  };
 
-    this.greeting = function(){
-        console.log(`Welcome ${this.username}`);
-
-    }
-
-    return this
+  return this;
 }
 
-const userOne = new User("hitesh", 12, true)
-const userTwo = new User("ChaiAurCode", 11, false)
+// new keyword is a constructor method to make a new multiple instance of the one object
+const userOne = new User("Kamal", 12, true);
+const userTwo = new User("Anmol", 11, false);
 console.log(userOne.constructor);
-console.log(userTwo);
+//  in this constructor gives refrence of function
+// console.log(userTwo);
+// console.log(userOne);
+
+//  when we use new keyword then
+/* step  1  create a new empty object
+step 2 call a constructor function with the value as an argument 
+step 3 inject the all value and variable in funtion and pass to function
+step 4 all work is done and give output
+*/
