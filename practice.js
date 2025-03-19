abf73d1c3102067f9b0acc58fa54c908e884df9d
kamalpandey27 <kamal0000027@gmail.com>
2025-03-19 10:39:18 +0530

    some code

---
+++ practice.js
@@ -0,0 +1,18 @@
+// const number = 123;
+
+// // Convert number to string and split into an array of digits
+// const digits = number.toString().split("");
+// console.log(digits);
+// // Find the largest digit
+// const largestDigit = Math.max(...digits.map(Number));
+
+// console.log(`The largest digit is ${largestDigit}`);
+
+let a = 350;
+let b = 400;
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+console.log(a, b);
